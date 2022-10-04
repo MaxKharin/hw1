d9333ea24ed2d630a935bce3f79897b226185307
MaximKharin <maximkharin@gmail.com>
2022-10-05 01:47:36 +0300

    finished task4 hw1

---
+++ task4.py
@@ -0,0 +1,15 @@
+# Напишите программу, которая по заданному номеру четверти,
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
+quart_num = int(input('Please, enter a quarter number: '))
+if 0 < quart_num < 5:
+    if quart_num == 1:
+        print('Possible dot coordinates are x > 0 and y > 0')
+    elif quart_num == 2:
+        print('Possible dot coordinates are x < 0 and y > 0')
+    elif quart_num == 3:
+        print('Possible dot coordinates are x < 0 and y < 0')
+    elif quart_num == 4:
+        print('Possible dot coordinates are x > 0 and y < 0')
+else:
+    print(quart_num, 'is not a quarter number! Please, enter an integer number from 1 to 4.')
